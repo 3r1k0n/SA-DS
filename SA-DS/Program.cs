b6735d47be544ccb6800fe52136ddcd8496a7484
3r1k0n <3r1k0n@gmail.com>
2015-12-11 18:27:55 +0100

    obavljen prvi korak

--- SA-DS/Program.cs
+++ SA-DS/Program.cs
@@ -10,6 +10,42 @@ namespace SA_DS
     {
         static void Main(string[] args)
         {
+            var S = "attagcgagcg$";
+            S = S.ToUpper();
+
+            // 1. Scan S once to classify all the characters as L - or S - type into t;
+            List<bool> t = CreateLSList(S);
+        }
+
+        private static List<bool> CreateLSList(string S)
+        {
+            // L/S list where S is true and L is false
+            List<bool> t = new List<bool>();
+
+            for (int i = S.Length - 1; i >= 0; i--)
+            {
+                if (S[i] == '$')
+                {
+                    t.Insert(0, true); //insert S for $
+                }
+                else
+                {
+                    if (S[i] > S[i + 1])
+                    {
+                        t.Insert(0, false); //insert L for S[i] > S[i + 1]
+                    }
+                    else if (S[i] < S[i + 1])
+                    {
+                        t.Insert(0, true); //insert S for S[i] < S[i + 1]
+                    }
+                    else
+                    {
+                        t.Insert(0, t[0]); //copy value if S[i] == S[i + 1]
+                    }
+                }
+            }
+
+            return t;
         }
     }
 }
