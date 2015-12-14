288caefb5172eef8188a1fcf7249ee4745684be0
3r1k0n <3r1k0n@gmail.com>
2015-12-14 16:26:33 +0100

    napisana funkcija CreateP1

--- SA-DS/Program.cs
+++ SA-DS/Program.cs
@@ -10,11 +10,13 @@ namespace SA_DS
     {
         static void Main(string[] args)
         {
-            var S = "attagcgagcg$";
+            var S = "mmiissiissiippii$";
             S = S.ToUpper();
 
             // 1. Scan S once to classify all the characters as L - or S - type into t;
             List<bool> t = CreateLSList(S);
+            // 2. Scan t once to find all the d-critical substringsin S into P1
+            List<int> P1 = CreateP1(t);
         }
 
         private static List<bool> CreateLSList(string S)
@@ -46,6 +48,29 @@ namespace SA_DS
             }
 
             return t;
+        }
+        private static List<int> CreateP1(List<bool> t)
+        {
+            // creates P1 array of pointers to d-critical chars
+            // RULES:
+            // - S[i] is d-critical only if it is a LMS or if  S[i-d] is d-critical and no char in between is critical
+            // - S[i] is LMS if S[i] is S type (true) and S[i-1] is L type (false)
+            // - first char is not critical, last char is critical
+            // - if S[i] is critical, S[i-1] and S[i+1] are not
+            // - d>=2, in this implementation d=2
+
+            List<int> P1 = new List<int>();
+            int d = 2;
+
+            for (int i = 1; i < t.Count; i++)
+            {
+                if ((t[i] == true && t[i - 1] == false) || P1.DefaultIfEmpty(-d).Last()==i-d)
+                    // if S[i] is critical or the last added d-critical char was S[i-d] (we compare indices)
+                {
+                    P1.Add(i);
+                }
+            }
+            return P1;
         }
     }
 }
